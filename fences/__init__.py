014548da2e74490867abec27965f04145a8b0cc9
LTMenezes <leonardotmenezes@gmail.com>
2024-09-13 20:00:41 +0200

    updates version

--- fences/__init__.py
+++ fences/__init__.py
@@ -1,5 +1,5 @@
 """Fences is an LLM augmented OpenAPI interpreter with a visual interface to easily make requests"""
 
-__version__ = "0.1.1"
+__version__ = "0.1.2"
 
 from .agent import Agent as Agent
